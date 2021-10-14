41af1904cdf35815dda119565ea1f4c96986e46b
Siddharth <siddharth.muscat@gmail.com>
2021-10-14 08:45:13 +0530

    TS => JS

--- index.ts
+++
@@ -1,9 +0,0 @@
-addEventListener('fetch', event => {
-	event.respondWith(Response.redirect(unwind(event.request), 302));
-});
-
-function unwind(request) {
-	const {pathname} = new URL(request.url);
-	console.log(pathname);
-	return 'https://dev.to';
-}
