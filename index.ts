76d054d00ea7daf408a928bc1b06183984acc50a
Siddharth <siddharth.muscat@gmail.com>
2021-10-14 08:33:22 +0530

    Redirect test

--- index.ts
+++ index.ts
@@ -1,9 +1,3 @@
 addEventListener('fetch', event => {
-	const response = new Response('Hello world!', {
-		headers: {
-			'Content-Type': 'text/plain'
-		}
-	});
-
-	event.respondWith(response);
+	event.respondWith(Response.redirect('https://dev.to', 302));
 });
