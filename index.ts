5f5566ad7307bc84bd4ed7fdf231ab1a02b7fcff
Siddharth <siddharth.muscat@gmail.com>
2021-10-14 08:31:00 +0530

    Actual hello world

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world!');
+addEventListener('fetch', event => {
+	const response = new Response('Hello world!', {
+		headers: {
+			'Content-Type': 'text/plain'
+		}
+	});
+
+	event.respondWith(response);
+});
