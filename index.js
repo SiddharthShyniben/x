dc09d111b779999df0f4aeccc753eef45c618526
Siddharth <siddharth.muscat@gmail.com>
2021-10-14 08:50:59 +0530

    Rename

---
+++ index.js
@@ -0,0 +1,19 @@
+addEventListener('fetch', event => {
+	event.respondWith(unwind(event.request));
+});
+
+function unwind(request) {
+	const {pathname} = new URL(request.url);
+
+	if (pathname === 'min') {
+		return new Response('<h1>WIP</h1>', {
+			headers: {
+				'content-type': 'text/html'
+			}
+		});
+	}
+
+	return redirect`https://siddu.tech`;
+}
+
+const redirect = ([where]) => Response.redirect(where, 302)
